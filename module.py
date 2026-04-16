beba8e078e13632c820cb2151bf1ee3e20394efb
Twig30 <immanuel.delgado@stu.vertexacademies.org>
2026-04-16 18:17:45 +0000

    added more code to module

--- module.py
+++ module.py
@@ -9,33 +9,45 @@ class PointOfSale:
   def __init__(self):
     self.checkout_total = 0 # This is an example varialbe, remove it or change it as you please.
     print("\nInitializing POS system...")
-  Cart = []
+    self.Cart = []
+    
+  
   def Show_cart(self):
-    for i in cart:
-      index = 0 
-      i = cart[index]
-      print(i)
-      index += 1 
-    if cart is []:
+    if len(self.Cart) == 0:
       print("Your cart is empty")
+      return
+    for i in range(0,len(self.Cart)):
+     print(self.Cart[i])
+
+  def Add_cart():
+    product = input("what product do you want to add?")
+    price = input("Enter the price of this item")
+    return
+    
+      
 
 
   
     
   def start(self):# This is the function that should be used to start the application. 
-    print("\n=================================")
-    print("MENU:Please choose an option seen below")
-    print("1. Show Cart")
-    print("2. Add To Cart")
-    print("3. Show Checkout Total")
-    print("4. Checkout")
-    print("5. EXIT")
-    print("=================================")
-
-    choice = input("\nEnter Selection:")
-    if choice == "1":
-      print("pulling up cart...")
-      self.Show_cart()
+    while True:
+      print("\n=================================")
+      print("\nMENU:Please choose an option seen below")
+      print("1. Show Cart")
+      print("2. Add To Cart")
+      print("3. Show Checkout Total")
+      print("4. Checkout")
+      print("5. EXIT")
+      print("=================================")
+      choice = input("\nSelection Entered:")
+      
+      if choice == "1":
+        print("pulling up cart...")
+        self.Show_cart()
+        
+        if choice == "2":
+          print("Pulling up product addtion panel...")
+          self.Add_cart()
 
 
 
