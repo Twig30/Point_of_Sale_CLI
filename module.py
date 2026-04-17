ee05ef9bf8c92a000c9cf1f8b247a6b4409d26f3
Twig30 <immanuel.delgado@stu.vertexacademies.org>
2026-04-17 18:09:45 +0000

    finalized code

--- module.py
+++ module.py
@@ -10,6 +10,7 @@ class PointOfSale:
     self.checkout_total = 0 # This is an example varialbe, remove it or change it as you please.
     print("\nInitializing POS system...")
     self.Cart = []
+    self.prices = []
     
   
   def Show_cart(self):
@@ -17,14 +18,22 @@ class PointOfSale:
       print("Your cart is empty")
       return
     for i in range(0,len(self.Cart)):
-     print(self.Cart[i])
+     print(self.Cart[i],": $",self.prices[i])
 
-  def Add_cart():
+  def Add_cart(self):
     product = input("what product do you want to add?")
-    price = input("Enter the price of this item")
+    self.Cart.append(product)
+    price = float(input("Enter the price of this item"))
+    self.prices.append(price)
+    print(product,price)
     return
     
-      
+  def Checkout_total(self):
+    output = 0
+    for i in range(len(self.prices)):
+      output += self.prices[i]  
+    print("Your Total is: $",output)
+
 
 
   
@@ -37,7 +46,6 @@ class PointOfSale:
       print("2. Add To Cart")
       print("3. Show Checkout Total")
       print("4. Checkout")
-      print("5. EXIT")
       print("=================================")
       choice = input("\nSelection Entered:")
       
@@ -45,9 +53,17 @@ class PointOfSale:
         print("pulling up cart...")
         self.Show_cart()
         
-        if choice == "2":
-          print("Pulling up product addtion panel...")
-          self.Add_cart()
+      if choice == "2":
+         print("Pulling up product addtion panel...")
+         self.Add_cart()
+      
+      if choice == "3":
+        print("Calculating Total...")
+        self.Checkout_total()
+      
+      if choice == "4":
+        print("Checking Out...")
+        break
 
 
 
